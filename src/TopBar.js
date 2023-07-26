3cf649c24535806a25878b91fdb011a1cbb3646d
jerry20201230 <76645282+jerry20201230@users.noreply.github.com>
2023-07-26 15:06:41 +0800

    添加icon
    -添加各頁主色icon

--- src/TopBar.js
+++ src/TopBar.js
@@ -5,7 +5,7 @@ import MapIcon from '@mui/icons-material/Map';
 import SwipeableDrawer from '@mui/material/SwipeableDrawer';
 import HomeIcon from '@mui/icons-material/Home';
 import SettingsIcon from '@mui/icons-material/Settings';
-import { useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
@@ -16,103 +16,121 @@ import { Link } from 'react-router-dom';
 
 function TopBar({ title }) {
   const [showNavigation, setShowNavigation] = useState(false);
-  useEffect(()=>{
+  useEffect(() => {
     document.title = `${title} - 大眾運輸查詢系統`
-  },[])
+  }, [])
   return (
     <>
-    
-    <Box>
-     <AppBar position="fixed" >
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-            onClick={() => setShowNavigation((prev) => !prev)}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            {title}
-          </Typography>
-          <IconButton color="inherit" component={Link} to="/map" href="/map"><MapIcon /></IconButton>
-        </Toolbar>
-      </AppBar> 
-      <Toolbar />
-      <SwipeableDrawer
-      sx={{zIndex:100000}}
-        anchor="left"
-        open={showNavigation}
-        onClose={() => setShowNavigation(false)}
-        onOpen={() => setShowNavigation(true)}>
 
-        <Box sx={{ width: '100%', minWidth: 240, bgcolor: 'background.paper' }}>
-          <nav aria-label="main mailbox folders">
-            <List>
-              <ListItem disablePadding>
-                <ListItemButton component={Link} to="/">
+      <Box>
+        <AppBar position="fixed" >
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+              onClick={() => setShowNavigation((prev) => !prev)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              {title}
+            </Typography>
+            <IconButton color="inherit" component={Link} to="/map" href="/map"><MapIcon /></IconButton>
+          </Toolbar>
+        </AppBar>
+        <Toolbar />
+        <SwipeableDrawer
+          sx={{ zIndex: 100000 }}
+          anchor="left"
+          open={showNavigation}
+          onClose={() => setShowNavigation(false)}
+          onOpen={() => setShowNavigation(true)}>
+
+          <Box sx={{ width: '100%', minWidth: 240, bgcolor: 'background.paper' }}>
+            <nav aria-label="main mailbox folders">
+              <List>
+                <ListItem disablePadding>
+                  <ListItemButton component={Link} to="/">
+                    <ListItemIcon>
+                      <HomeIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="首頁" />
+                  </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                  <ListItemButton component={Link} to="/map">
+                    <ListItemIcon>
+                      <MapIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="地圖" />
+                  </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                  <ListItemButton component={Link} to="/setting">
+                    <ListItemIcon>
+                      <SettingsIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="設定" />
+                  </ListItemButton>
+                </ListItem>
+              </List>
+            </nav>
+            <Divider />
+            <nav>
+              <List>
+
+                <ListItem disablePadding>
+                  <ListItemButton component={Link} to="/tra">
+                    <ListItemIcon>
+                      <Typography sx={{ mr: 1, display: "inline-block", width: "1.5rem", height: "1.5rem", borderRadius: "5px", verticalAlign: "text-top", background: "linear-gradient(315deg, #004da7, #7fa9d9)" }} variant='div' ></Typography>
+                    </ListItemIcon>
+                    <ListItemText primary="台鐵" />
+                  </ListItemButton>
+                </ListItem>
+
+                <ListItem disablePadding>
+                  <ListItemButton component={Link} to="/hsr">
+                    <ListItemIcon>
+                      <Typography sx={{ mr: 1, display: "inline-block", width: "1.5rem", height: "1.5rem", borderRadius: "5px", verticalAlign: "text-top", background: "linear-gradient(315deg, #ca4f0f, #f89867)" }} variant='div' ></Typography>
+                    </ListItemIcon>
+                    <ListItemText primary="高鐵" />
+                  </ListItemButton>
+                </ListItem>
+
+                <ListItem disablePadding>
+                  <ListItemButton component={Link} to="/mrt">
+                    <ListItemIcon>
+                      <Typography sx={{ mr: 1, display: "inline-block", width: "1.5rem", height: "1.5rem", borderRadius: "5px", verticalAlign: "text-top", background: "linear-gradient(315deg, #8dc21f,#ccf871)" }} variant='div' ></Typography>
+                    </ListItemIcon>
+                    <ListItemText primary="捷運" />
+                  </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                  <ListItemButton component={Link} to="/bus">
                   <ListItemIcon>
-                    <HomeIcon />
-                  </ListItemIcon>
-                  <ListItemText primary="首頁" />
-                </ListItemButton>
-              </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton component={Link} to="/map">
+                      <Typography sx={{ mr: 1, display: "inline-block", width: "1.5rem", height: "1.5rem", borderRadius: "5px", verticalAlign: "text-top", background: "linear-gradient(315deg, #8d8d8d,#ccc)" }} variant='div' ></Typography>
+                    </ListItemIcon>
+                    <ListItemText primary="公車" />
+                  </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                  <ListItemButton component={Link} to="/bike">
                   <ListItemIcon>
-                    <MapIcon />
-                  </ListItemIcon>
-                  <ListItemText primary="地圖" />
-                </ListItemButton>
-              </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton component={Link} to="/setting">
-                  <ListItemIcon>
-                    <SettingsIcon />
-                  </ListItemIcon>
-                  <ListItemText primary="設定" />
-                </ListItemButton>
-              </ListItem>
-            </List>
-          </nav>
-          <Divider />
-          <nav>
-            <List>
-              <ListItem disablePadding>
-                <ListItemButton component={Link} to="/tra">
-                  <ListItemText primary="台鐵" />
-                </ListItemButton>
-              </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton component={Link} to="/hsr">
-                  <ListItemText primary="高鐵" />
-                </ListItemButton>
-              </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton component={Link} to="/mrt">
-                  <ListItemText primary="捷運" />
-                </ListItemButton>
-              </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton component={Link} to="/bus">
-                  <ListItemText primary="公車" />
-                </ListItemButton>
-              </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton component={Link} to="/bike">
-                  <ListItemText primary="公共自行車" />
-                </ListItemButton>
-              </ListItem>
-            </List>
-          </nav>
-        </Box>
+                      <Typography sx={{ mr: 1, display: "inline-block", width: "1.5rem", height: "1.5rem", borderRadius: "5px", verticalAlign: "text-top", background: "linear-gradient(315deg, #ffef00,#fffdd8)" }} variant='div' ></Typography>
+                    </ListItemIcon>
+                    <ListItemText primary="公共自行車" />
+                  </ListItemButton>
+                </ListItem>
+              </List>
+            </nav>
+          </Box>
 
 
-      </SwipeableDrawer>
-    </Box>
+        </SwipeableDrawer>
+      </Box>
     </>
   );
 }

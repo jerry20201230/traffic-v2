41b777ee689d15cb1b8778a72ea27a8b04cc32cd
jerry20201230 <76645282+jerry20201230@users.noreply.github.com>
2023-08-15 16:18:37 +0800

    更新附近大眾運輸tabs(連接到地圖)
    -將tabs內的地點連接到地圖
    -美化顯示與排版

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -10,7 +10,7 @@ import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import LocateControl from '../location';
 import Paper from '@mui/material/Paper';
-import { Box, Autocomplete, TextField, Button, IconButton } from '@mui/material';
+import { Box, Autocomplete, TextField, Button, IconButton, Alert } from '@mui/material';
 import PropTypes from 'prop-types';
 import { Global } from '@emotion/react';
 import { styled } from '@mui/material/styles';
@@ -19,7 +19,7 @@ import { grey } from '@mui/material/colors';
 import Skeleton from '@mui/material/Skeleton';
 import Typography from '@mui/material/Typography';
 import SwipeableDrawer from '@mui/material/SwipeableDrawer';
-import { RoomPreferencesTwoTone } from '@mui/icons-material';
+import { FunctionsOutlined, RoomPreferencesTwoTone } from '@mui/icons-material';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import { CircularProgress } from '@mui/material';
@@ -31,15 +31,15 @@ import LocationOnIcon from '@mui/icons-material/LocationOn';
 import NearMeIcon from '@mui/icons-material/NearMe';
 
 function Map() {
-
+  const [locType, setLocType] = React.useState("你的位置資訊")
   const [dialogOpen, setDialogOpen] = React.useState(false);
   const [locationMark, setLocationMark] = React.useState()
   const [locationNear, setLocationNear] = React.useState(<><NearMeIcon sx={{ verticalAlign: "bottom" }} /> 資料讀取中 <CircularProgress size="1rem" sx={{ verticalAlign: "baseline" }} /></>)
   const [locationSummery, setLocationSummery] = React.useState(<><NotListedLocationIcon sx={{ verticalAlign: "bottom" }} /> 資料讀取中 <CircularProgress size="1rem" sx={{ verticalAlign: "baseline" }} /></>)
   const [locationXY, setLocationXY] = React.useState([])
   const mymap = React.useRef()
-
-  var redIcon = new L.Icon({
+  var mapLoaded = false
+  const redIcon = new L.Icon({
     iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
     shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
     iconSize: [25, 41],
@@ -47,8 +47,19 @@ function Map() {
     popupAnchor: [1, -34],
     shadowSize: [41, 41]
   });
-
-
+  const greenIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
+  function UrlParam(name) {
+    var url = new URL(window.location.href),
+      result = url.searchParams.get(name);
+    return result;
+  }
 
   function getLocation() {
     if (navigator.geolocation) {
@@ -93,11 +104,37 @@ function Map() {
   }
 
   React.useEffect(() => {
-    getLocation()
-  }, [])
 
 
+    if (!UrlParam("lat") || !UrlParam("lon") || !UrlParam("popup")) {
+      getLocation(mymap)
 
+    }
+    else {
+      var lat = Number(UrlParam("lat")), lon = Number(UrlParam("lon"))
+      setLocationXY([lat, lon])
+      setLocType(<>此地點的位置資訊<Alert severity="warning">定位僅供參考</Alert></>)
+      console.log(mymap)
+
+      if (mymap.current !== null || mapLoaded) {
+
+        mymap.current.setView(
+          [lat, lon], 16
+        )
+        let mark = L.marker([lat, lon], {
+          icon: greenIcon
+        }).addTo(mymap.current)
+        mark.bindPopup(UrlParam("popup"))
+        getData(`https://tdx.transportdata.tw/api/advanced/V3/Map/GeoLocating/Address/LocationX/${lon}/LocationY/${lat}?%24format=JSON`, (res) => setLocationSummery(<><LocationOnIcon sx={{ verticalAlign: "bottom" }} /> {res[0].Address}</>), { useLocalCatch: true })
+        getData(`https://tdx.transportdata.tw/api/advanced/V3/Map/GeoLocating/Markname/LocationX/${lon}/LocationY/${lat}?%24format=JSON`, (res) => setLocationNear(<><NearMeIcon sx={{ verticalAlign: "bottom" }} /> {res[0].Distance > 0 ? `${res[0].Markname} 附近 (${Math.round(res[0].Distance)}公尺)` : `${res[0].Markname}`} </>), { useLocalCatch: true })
+      } else {
+
+      }
+
+    }
+
+
+  }, [mymap.current])
 
 
 
@@ -132,6 +169,11 @@ function Map() {
       setOpen(newOpen);
       localStorage.setItem("map.drawer.show", newOpen === true ? "1" : "0")
     };
+
+    function closeDrawer() {
+      setOpen(false)
+      localStorage.setItem("map.drawer.show", "0")
+    }
 
     return (
 
@@ -177,15 +219,14 @@ function Map() {
             sx={{
               px: 2,
               pb: 2,
-
               overflow: 'auto',
             }}
           >
-            <h4>你的位置資訊</h4>
+            <h4>{locType}</h4>
             {locationSummery}<br />
             {locationNear}
-            <h4>附近大眾運輸(500公尺)</h4>
-            <BasicTabs lat={locationXY[0]} lon={locationXY[1]} />
+            <h4>附近大眾運輸(500公尺以內)</h4>
+            <BasicTabs lat={locationXY[0]} lon={locationXY[1]} data={{ "map": mymap, markedCallback: closeDrawer }} />
           </StyledBox>
         </SwipeableDrawer>
       </Root>
@@ -211,7 +252,8 @@ function Map() {
       <div style={{ display: "flex", flexFlow: "column", height: "100%" }}>
         <TopBar title="地圖" />
         <div className="map" id="map" style={{ width: "100%", height: `100%`, flexGrow: 1 }}>
-          <MapContainer ref={mymap} center={[23.75518176611264, 120.9406086935125]} zoom={7} style={{ width: "100%", height: "100%" }}>
+          <MapContainer ref={mymap}
+            center={[23.75518176611264, 120.9406086935125]} zoom={7} style={{ width: "100%", height: "100%" }}>
             <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
--- src/tabs.jsx
+++ src/tabs.jsx
@@ -6,6 +6,13 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import getData from './getData';
 import { useLocation } from 'react-router';
+import Paper from '@mui/material/Paper';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import { Button } from '@mui/material';
+import L from 'leaflet'
+import { Link } from 'react-router-dom';
 
 function CustomTabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -46,6 +53,17 @@ function convertOperator(text) {
 }
 var trainStation_isTransferStation = false
 export default function BasicTabs({ lat, lon, spec, hide, data, children }) {
+  const greenIcon = new L.Icon({
+    iconUrl:
+      "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
+    shadowUrl:
+      "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41],
+  });
+
   const [value, setValue] = React.useState(0);
 
   const [tabsDoc, setTabsDoc] = React.useState((<></>))
@@ -62,6 +80,7 @@ export default function BasicTabs({ lat, lon, spec, hide, data, children }) {
 
 
   React.useEffect(() => {
+
     if (lat && lon) {
       getData(`https://tdx.transportdata.tw/api/advanced/V3/Map/GeoLocating/Transit/Nearby/LocationX/${lon}/LocationY/${lat}/Distance/500?%24format=JSON`, (res) => {
         console.log(res)
@@ -73,14 +92,16 @@ export default function BasicTabs({ lat, lon, spec, hide, data, children }) {
 
 
   React.useEffect(() => {
-
-
     if (spec === "tra") {
       getData("https://tdx.transportdata.tw/api/basic/v3/Rail/TRA/LineTransfer?%24format=JSON", (res) => {
         for (let i = 0; i < res.LineTransfers.length; i++) {
           if (res.LineTransfers[i].FromStationID === data.stationID) {
             trainStation_isTransferStation = true
-            traTab[0] = <>{res.LineTransfers[i].FromLineName.Zh_tw.replace("西部幹線", "山線").replace("西部幹線 (海線)", "海線")} <br /> {res.LineTransfers[i].ToLineName.Zh_tw.replace("西部幹線", "山線").replace("西部幹線 (海線)", "海線")}</>
+            traTab[0] =
+              <>
+                <Typography sx={{ mr: 1, display: "inline-block", width: "1.5rem", height: "1.5rem", borderRadius: "5px", verticalAlign: "bottom", background: "linear-gradient(315deg, #004da7, #7fa9d9)" }} variant='div' ></Typography>{res.LineTransfers[i].FromLineName.Zh_tw.replace("西部幹線 (海線)", "海線").replace("西部幹線", "山線")} <p></p>
+                <Typography sx={{ mr: 1, display: "inline-block", width: "1.5rem", height: "1.5rem", borderRadius: "5px", verticalAlign: "bottom", background: "linear-gradient(315deg, #004da7, #7fa9d9)" }} variant='div' ></Typography>{res.LineTransfers[i].ToLineName.Zh_tw.replace("西部幹線 (海線)", "海線").replace("西部幹線", "山線")}
+              </>
             break
           }
         }
@@ -92,11 +113,62 @@ export default function BasicTabs({ lat, lon, spec, hide, data, children }) {
 
     }
     else {
-      console.log(nearByData[0].RailStations.RailStationList[0])
-
       for (let i = 0; i < nearByData[0].RailStations.RailStationList.length; i++) {
         if (nearByData[0].RailStations.RailStationList[i].StationUID.includes("TRA")) {
-          traTab[0] = <> <span>{traTab[0]} {nearByData[0].RailStations.RailStationList[i].StationName}車站</span><br /></>
+          if (traTab[0] === "無資料") {
+            traTab[0] =
+              <>
+                <Card sx={{ mt: 0, pt: 0 }}>
+                  <CardContent>
+                    <Typography variant="h5" component="div">
+                      <Typography sx={{ mr: 1, display: "inline-block", width: "1.5rem", height: "1.5rem", borderRadius: "5px", verticalAlign: "text-top", background: "linear-gradient(315deg, #004da7, #7fa9d9)" }} variant='div' ></Typography> {nearByData[0].RailStations.RailStationList[i].StationName}車站
+                    </Typography>
+                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+
+                    </Typography>
+                    <Typography variant="body2" component="div">
+                      {data.map ?
+                        <Button variant="contained" onClick={() => {
+                          let marker = L.marker([nearByData[0].RailStations.RailStationList[i].LocationY, nearByData[0].RailStations.RailStationList[i].LocationX], { icon: greenIcon }).addTo(data.map.current);
+                          data.markedCallback(false);
+                          marker.bindPopup(nearByData[0].RailStations.RailStationList[i].StationName + "車站")
+                          data.map.current.setView([nearByData[0].RailStations.RailStationList[i].LocationY, nearByData[0].RailStations.RailStationList[i].LocationX], 16)
+                        }}>在地圖上顯示</Button>
+                        :
+                        <Button variant="contained" component={Link} to={`/map/?lat=${nearByData[0].RailStations.RailStationList[i].LocationY}&lon=${nearByData[0].RailStations.RailStationList[i].LocationX}&popup=${nearByData[0].RailStations.RailStationList[i].StationName}車站`}>在地圖上顯示</Button>
+                      }
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </>
+          } else {
+            traTab[0] =
+              <>
+                {traTab[0]}
+                <Card sx={{ mt: 0, pt: 0 }}>
+                  <CardContent>
+                    <Typography variant="h5" component="div">
+                      <Typography sx={{ mr: 1, display: "inline-block", width: "1.5rem", height: "1.5rem", borderRadius: "5px", verticalAlign: "text-top", background: "linear-gradient(315deg, #004da7, #7fa9d9)" }} variant='div' ></Typography> {nearByData[0].RailStations.RailStationList[i].StationName}車站
+                    </Typography>
+                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+
+                    </Typography>
+                    <Typography variant="body2" component="div">
+                      {data.map ?
+                        <Button variant="contained" onClick={() => {
+                          let marker = L.marker([nearByData[0].RailStations.RailStationList[i].LocationY, nearByData[0].RailStations.RailStationList[i].LocationX], { icon: greenIcon }).addTo(data.map.current);
+                          data.markedCallback(false);
+                          marker.bindPopup(nearByData[0].RailStations.RailStationList[i].StationName + "車站")
+                          data.map.current.setView([nearByData[0].RailStations.RailStationList[i].LocationY, nearByData[0].RailStations.RailStationList[i].LocationX], 16)
+                        }}>在地圖上顯示</Button>
+                        :
+                        <Button variant="contained" component={Link} to={`/map/?lat=${nearByData[0].RailStations.RailStationList[i].LocationY}&lon=${nearByData[0].RailStations.RailStationList[i].LocationX}&popup=${nearByData[0].RailStations.RailStationList[i].StationName}車站`}>在地圖上顯示</Button>
+                      }
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </>
+          }
         }
       }
       if (traTab[0] === <></> || traTab === undefined || traTab.length < 1) {
@@ -113,9 +185,58 @@ export default function BasicTabs({ lat, lon, spec, hide, data, children }) {
       for (let i = 0; i < nearByData[0].RailStations.RailStationList.length; i++) {
         if (nearByData[0].RailStations.RailStationList[i].StationUID.includes("THSR")) {
           if (hsrTab[0] === "無資料") {
-            hsrTab[0] = <> <span>高鐵{nearByData[0].RailStations.RailStationList[i].StationName}站</span><br /></>
+            hsrTab[0] =
+              <>
+                <Card sx={{ mt: 0, pt: 0 }}>
+                  <CardContent>
+                    <Typography variant="h5" component="div">
+                      <Typography sx={{ mr: 1, display: "inline-block", width: "1.5rem", height: "1.5rem", borderRadius: "5px", verticalAlign: "text-top", background: "linear-gradient(315deg, #ca4f0f, #f89867)" }} variant='div' ></Typography> 高鐵{nearByData[0].RailStations.RailStationList[i].StationName}站
+                    </Typography>
+                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+
+                    </Typography>
+                    <Typography variant="body2" component="div">
+                      {data.map ?
+                        <Button variant="contained" onClick={() => {
+                          let marker = L.marker([nearByData[0].RailStations.RailStationList[i].LocationY, nearByData[0].RailStations.RailStationList[i].LocationX], { icon: greenIcon }).addTo(data.map.current);
+                          data.markedCallback(false);
+                          marker.bindPopup("高鐵" + nearByData[0].RailStations.RailStationList[i].StationName + "站")
+                          data.map.current.setView([nearByData[0].RailStations.RailStationList[i].LocationY, nearByData[0].RailStations.RailStationList[i].LocationX], 16)
+                        }}>在地圖上顯示</Button>
+                        :
+                        <Button variant="contained" component={Link} to={`/map/?lat=${nearByData[0].RailStations.RailStationList[i].LocationY}&lon=${nearByData[0].RailStations.RailStationList[i].LocationX}&popup=高鐵${nearByData[0].RailStations.RailStationList[i].StationName}站`}>在地圖上顯示</Button>
+                      }
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </>
           } else {
-            hsrTab[0] = <> <span>{hsrTab[0]} 高鐵{nearByData[0].RailStations.RailStationList[i].StationName}站</span><br /></>
+            hsrTab[0] =
+              <>
+                {hsrTab[0]}
+                <Card sx={{ mt: 0, pt: 0 }}>
+                  <CardContent>
+                    <Typography variant="h5" component="div">
+                      <Typography sx={{ mr: 1, display: "inline-block", width: "1.5rem", height: "1.5rem", borderRadius: "5px", verticalAlign: "text-top", background: "linear-gradient(315deg, #ca4f0f, #f89867)" }} variant='div' ></Typography> 高鐵{nearByData[0].RailStations.RailStationList[i].StationName}站
+                    </Typography>
+                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+
+                    </Typography>
+                    <Typography variant="body2" component="div">
+                      {data.map ?
+                        <Button variant="contained" onClick={() => {
+                          let marker = L.marker([nearByData[0].RailStations.RailStationList[i].LocationY, nearByData[0].RailStations.RailStationList[i].LocationX], { icon: greenIcon }).addTo(data.map.current);
+                          data.markedCallback(false);
+                          marker.bindPopup("高鐵" + nearByData[0].RailStations.RailStationList[i].StationName + "站")
+                          data.map.current.setView([nearByData[0].RailStations.RailStationList[i].LocationY, nearByData[0].RailStations.RailStationList[i].LocationX], 16)
+                        }}>在地圖上顯示</Button>
+                        :
+                        <Button variant="contained" component={Link} to={`/map/?lat=${nearByData[0].RailStations.RailStationList[i].LocationY}&lon=${nearByData[0].RailStations.RailStationList[i].LocationX}&popup=高鐵${nearByData[0].RailStations.RailStationList[i].StationName}站`}>在地圖上顯示</Button>
+                      }
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </>
           }
         }
       }

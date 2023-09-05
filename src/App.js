4e46c480f97011ad6a9c897dda7839cf2193721e
jerry20201230 <76645282+jerry20201230@users.noreply.github.com>
2023-09-05 20:42:22 +0800

    修正快取資料的bug

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ import { useEffect } from 'react';
 import { Welcome } from './pages/Welcome';
 
 function App() {
-  const CURRENT_VER = "1.0.1"
+  const CURRENT_VER = "1.0.2"
   return (
     <>
       {(!localStorage.getItem("ver") || localStorage.getItem("ver") !== CURRENT_VER) ? <Welcome CURRENT_VER={CURRENT_VER} /> :
--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -7,10 +7,8 @@ import LinearProgressWithLabel from '@mui/material/LinearProgress';
 
 export function Welcome({ next, title, CURRENT_VER }) {
     var allItem = [
-        "https://tdx.transportdata.tw/api/basic/v2/Basic/City?%24format=JSON",
         "https://tdx.transportdata.tw/api/basic/v2/Rail/TRA/Station?%24format=JSON",
         "https://tdx.transportdata.tw/api/basic/v2/Rail/THSR/Station?%24format=JSON",
-
     ]
     var item = 0
     var [summery, setSummery] = React.useState("")
@@ -19,37 +17,49 @@ export function Welcome({ next, title, CURRENT_VER }) {
 
     React.useEffect(() => {
         if (linkBtn.current) {
-            for (let i = 0; i < allItem.length; i++) {
-                console.log(i)
-                setSummery(`正在更新第 ${item + 1} 項資料，共 ${allItem.length} 項`)
-                getData(allItem[i], function (res) {
+            setSummery(`正在更新 全台縣市列表`)
+            getData("https://tdx.transportdata.tw/api/basic/v2/Basic/City?%24format=JSON", function (res) {
 
-                    if (allItem[i] === "https://tdx.transportdata.tw/api/basic/v2/Basic/City?%24format=JSON") {
+                setSummery(`全台縣市列表 更新成功，將依據此列表繼續下載其他資料...`)
+                for (let j = 0; j < res.length; j++) {
+                    allItem.push(`https://tdx.transportdata.tw/api/basic/v2/Bus/Route/City/${res[j].City}?%24format=JSON`)
+                }
+                for (let i = 0; i < allItem.length; i++) {
+                    console.log(i)
+                    setSummery(`正在更新第 ${item + 1} 項資料，共 ${allItem.length} 項`)
+                    getData(allItem[i], function (res) {
 
-                        setSummery(`全台縣市列表 (第${i}項) 更新成功，將依據此列表繼續下載其他${res.length}項資料...`)
-                        for (let j = 0; j < res.length; j++) {
-                            allItem.push(`https://tdx.transportdata.tw/api/basic/v2/Bus/Route/City/${res[j].City}?%24format=JSON`)
+                        if (allItem[i] === "https://tdx.transportdata.tw/api/basic/v2/Basic/City?%24format=JSON") {
+
+                            setSummery(`全台縣市列表 (第${i}項) 更新成功，將依據此列表繼續下載其他${res.length}項資料...`)
+                            for (let j = 0; j < res.length; j++) {
+                                allItem.push(`https://tdx.transportdata.tw/api/basic/v2/Bus/Route/City/${res[j].City}?%24format=JSON`)
+                            }
+                            item += 1
+                        } else {
+                            item += 1
+                            console.log("ITEM", item, (item / allItem.length) * 100)
+                            if (item + 1 < allItem.length) {
+                                setSummery(`第 ${item} 項資料更新成功，共 ${allItem.length} 項`)
+                            }
+                            else {
+                                setSummery("資料更新完畢")
+                                localStorage.setItem("ver", CURRENT_VER)
+                                linkBtn.current.click()
+                                window.location.reload()
+                            }
                         }
-                        item += 1
-                    } else {
-                        item += 1
-                        console.log("ITEM", item, (item / allItem.length) * 100)
-                        if (item + 1 < allItem.length) {
-                            setSummery(`第 ${item} 項資料更新成功，共 ${allItem.length} 項`)
-                        }
-                        else {
-                            setSummery("資料更新完畢")
-                            localStorage.setItem("ver", CURRENT_VER)
-                            linkBtn.current.click()
-                            window.location.reload()
-                        }
-                    }
 
 
-                }, {
-                    useLocalCatch: true,
-                })
-            }
+                    }, {
+                        useLocalCatch: true,
+                    })
+                }
+
+            }, {
+                useLocalCatch: true,
+            })
+
         }
     }, [linkBtn])
 
